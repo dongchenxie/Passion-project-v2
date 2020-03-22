9a72157d222658d6a6e5dd83c946177b97b70303
xdc811 <xdc811@users.noreply.github.com>
2020-03-22 14:11:50 -0700

    init

---
+++ Passion-project-portfolio/Passion-project-portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Passion_project_portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
